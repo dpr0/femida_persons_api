41e1e472dbcf2ecec6e59b6db32db919a6e9cc90
dvitvitskiy <dvitvitskiy.pro@gmail.com>
2023-08-18 08:56:20 +0300

    dev

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :rbenv_ruby,      '3.0.5'
 set :application,     'femida_persons_api'
 set :repo_url,        'git@github.com:dpr0/femida_persons_api.git' # 'gitlab.com:femida1/femida_persons_api.git'
 set :linked_files,    fetch(:linked_files, []).push('config/cable.yml', 'config/database.yml', 'config/secrets.yml', 'config/master.key', 'config/credentials.yml.enc', '.env')
-set :linked_dirs,     fetch(:linked_dirs, []).push('log', 'tmp/parser', 'tmp/narod', 'tmp/itog_1', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'tmp/session_store', 'vendor/bundle', 'public/system', 'public/uploads')
+set :linked_dirs,     fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'tmp/session_store', 'vendor/bundle', 'public/system', 'public/uploads')
 set :keep_releases,   5
 set :user,            'support'
 set :use_sudo,        false
